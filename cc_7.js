331cc9386e67649ab3d282b05e4b77f8369bb11f
tiighe <tigheknowles@gmail.com>
2026-03-08 23:03:16 -0400

    added button event to update cta headline

--- cc_7.js
+++ cc_7.js
@@ -1,3 +1,12 @@
 const input = document.getElementById("headlineInput");
 const button = document.getElementById("updateButton");
 const cta = document.getElementById("cta");
+
+button.addEventListener("click", function () {
+  const newText = input.value.trim();
+
+  if (newText !== "") {
+    cta.textContent = newText;
+    input.value = "";
+  }
+});
